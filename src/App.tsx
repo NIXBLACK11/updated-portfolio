45ff3ab1e830486ed5cf04fee5f09fb4a3a6e159
NIXBLACK11 <Siddharthsinghrana11@gmail.com>
2024-09-23 15:39:02 +0530

    analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import { Landing } from './Pages/Landing';
 import MouseLight from './components/MouseLight';
 import { AllProjects } from './Pages/AllProjects';
@@ -7,6 +8,7 @@ import { AllProjects } from './Pages/AllProjects';
 function App() {
   return (
     <div className='bg-[#000000] min-h-screen'>
+      <Analytics/>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Landing />} />
