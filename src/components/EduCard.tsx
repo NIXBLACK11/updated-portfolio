5add8b74eb1cb7e61df97900dd263e7ab11ecfdd
NIXBLACK11 <siddharthsinghrana11@gmail.com>
2025-01-09 20:01:04 +0530

    education font change

--- src/components/EduCard.tsx
+++ src/components/EduCard.tsx
@@ -16,7 +16,7 @@ export const EduCard: React.FC<EduCardProps> = ({
 }) => {
   return (
     <TiltWrapper options={{ max: 5, speed: 10 }} className='mt-10 rounded-lg w-full'>
-      <a href={link} target='_blank' className="rounded-lg block w-full">
+      <a href={link} target='_blank' className="font-custom rounded-lg block w-full">
         <div className="flex flex-col items-start w-full">
           <span className="secondary-color custom-font text-sm mb-2">{dateRange}</span>
           <h3 className="font-color text-xl font-custom mb-1">{course}</h3>
